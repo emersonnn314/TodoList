dc026329a9d8404cfd5e54c3c5bb1cbcf69d15ad
Emerson <emersonwhiskey@protonmail.com>
2024-02-06 18:40:58 -0600

    deleting unecessary files

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using TodoList;
@@ -9,7 +10,20 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped<LocalStorageAccessor>();
-
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddBlazoredLocalStorageAsSingleton();
+
+
+
+//public static async Task Main(string[] args)
+//{
+//    var builder = WebAssemblyHostBuilder.CreateDefault(args);
+//    builder.RootComponents.Add<App>("app");
+
+//    builder.Services.AddBlazoredLocalStorage();
+
+//    await builder.Build().RunAsync();
+//}
 
 await builder.Build().RunAsync();
+
--- TodoItem.cs
+++ TodoItem.cs
@@ -2,9 +2,15 @@
 {
     public class TodoItem
     {
+        public Guid Id { get; set; }
         public string? Title { get; set; }
         public bool IsDone { get; set; }
 
         public int Priority { get; set; }
+
+        public TodoItem()
+        {
+            Id = Guid.NewGuid();
+        }
     }
 }
