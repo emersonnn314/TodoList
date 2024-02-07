07a90351e79e01f1df236e613b4af1914195dc16
Emerson <emersonwhiskey@protonmail.com>
2024-02-06 18:45:02 -0600

    removing unecessary stuff

--- Program.cs
+++ Program.cs
@@ -9,21 +9,8 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped<LocalStorageAccessor>();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddBlazoredLocalStorageAsSingleton();
-
-
-
-//public static async Task Main(string[] args)
-//{
-//    var builder = WebAssemblyHostBuilder.CreateDefault(args);
-//    builder.RootComponents.Add<App>("app");
-
-//    builder.Services.AddBlazoredLocalStorage();
-
-//    await builder.Build().RunAsync();
-//}
 
 await builder.Build().RunAsync();
 
